d04aae388ae2b421d9d23367dba2ad46f56a10b9
Austin <copeman.austin@gmail.com>
2016-10-13 19:59:04 -0400

    Updates

--- java/src/odrive/SerialComm.java
+++ java/src/odrive/SerialComm.java
@@ -1,108 +1,93 @@
 package odrive;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import gnu.io.CommPortIdentifier; 
-import gnu.io.SerialPort;
-import gnu.io.SerialPortEvent; 
-import gnu.io.SerialPortEventListener; 
-import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import jssc.SerialPort;
+import static jssc.SerialPort.MASK_RXCHAR;
+import jssc.SerialPortEvent;
+import jssc.SerialPortException;
+import jssc.SerialPortList;
+
 /**
  * 
- * @author mr_co_000
+ * @author http://arduino-er.blogspot.com/2015/09/example-of-using-jssc-communicate.html
  */
-public class SerialComm implements SerialPortEventListener {
-	SerialPort serialPort;
-        /** The port we're normally going to use. */
-	private static final String PORT_NAMES[] = { 
-			"/dev/tty.usbserial-A9007UX1", // Mac OS X
-                        "/dev/ttyACM0", // Raspberry Pi
-			"/dev/ttyUSB0", // Linux
-			"COM3", // Windows
-	};
-	/**
-	* A BufferedReader which will be fed by a InputStreamReader 
-	* converting the bytes into characters 
-	* making the displayed results codepage independent
-	*/
-	private BufferedReader input;
-	/** The output stream to the port */
-	private OutputStream output;
-	/** Milliseconds to block while waiting for port open */
-	private static final int TIME_OUT = 2000;
-	/** Default bits per second for COM port. */
-	private static final int DATA_RATE = 9600;
+public class SerialComm {
+    
+    private SerialPort arduinoPort = null;
+    private ObservableList<String> portList = FXCollections.observableArrayList();
+    private String[] serialPortNames = SerialPortList.getPortNames();
+    
+    /*
+     *Detects the connected ports and stores in an obervable array list 
+     *Does not recheck ports if it gets disconnected
+     */ 
+    public void detectPort(){
+        for(String name : serialPortNames){
+            portList.add(name);
+        }
+    }
+    
+           
+    public boolean connectArduino(String port){
+        
+        System.out.println("connectArduino");
+        
+        boolean success = false;
+        SerialPort serialPort = new SerialPort(port);
+        try {
+            serialPort.openPort();
+            serialPort.setParams(
+                    SerialPort.BAUDRATE_115200,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+            serialPort.setEventsMask(MASK_RXCHAR);
+            serialPort.addEventListener((SerialPortEvent serialPortEvent) -> {
+                if(serialPortEvent.isRXCHAR()){
+                    try {
+                        String st = serialPort.readString(serialPortEvent.getEventValue());
+                        System.out.println(st);    
+                    } catch (SerialPortException ex) {
+                        Logger.getLogger(SerialComm.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    
+                }
+            });
+            
+            arduinoPort = serialPort;
+            success = true;
+        } catch (SerialPortException ex) {
+            Logger.getLogger(SerialComm.class.getName())
+                    .log(Level.SEVERE, null, ex);
+            System.out.println("SerialPortException: " + ex.toString());
+        }
 
-	public void initialize() {
-                // the next line is for Raspberry Pi and 
-                // gets us into the while loop and was suggested here was suggested http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
-                System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
-
-		CommPortIdentifier portId = null;
-		Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
-
-		//First, Find an instance of serial port as set in PORT_NAMES.
-		while (portEnum.hasMoreElements()) {
-			CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
-			for (String portName : PORT_NAMES) {
-				if (currPortId.getName().equals(portName)) {
-					portId = currPortId;
-					break;
-				}
-			}
-		}
-		if (portId == null) {
-			System.out.println("Could not find COM port.");
-			return;
-		}
-
-		try {
-			// open serial port, and use class name for the appName.
-			serialPort = (SerialPort) portId.open(this.getClass().getName(),
-					TIME_OUT);
-
-			// set port parameters
-			serialPort.setSerialPortParams(DATA_RATE,
-					SerialPort.DATABITS_8,
-					SerialPort.STOPBITS_1,
-					SerialPort.PARITY_NONE);
-
-			// open the streams
-			input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
-			output = serialPort.getOutputStream();
-
-			// add event listeners
-			serialPort.addEventListener(this);
-			serialPort.notifyOnDataAvailable(true);
-		} catch (Exception e) {
-			System.err.println(e.toString());
-		}
-	}
-
-	/**
-	 * This should be called when you stop using the port.
-	 * This will prevent port locking on platforms like Linux.
-	 */
-	public synchronized void close() {
-		if (serialPort != null) {
-			serialPort.removeEventListener();
-			serialPort.close();
-		}
-	}
-
-	/**
-	 * Handle an event on the serial port. Read the data and print it.
-	 */
-	public synchronized void serialEvent(SerialPortEvent oEvent) {
-		if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
-			try {
-				String inputLine=input.readLine();
-				System.out.println(inputLine);
-			} catch (Exception e) {
-				System.err.println(e.toString());
-			}
-		}
-		// Ignore all the other eventTypes, but you should consider the other ones.
-	}
+        return success;
+    }
+    
+    public void disconnectArduino(){
+        
+        System.out.println("disconnectArduino()");
+        if(arduinoPort != null){
+            try {
+                arduinoPort.removeEventListener();
+                
+                if(arduinoPort.isOpened()){
+                    arduinoPort.closePort();
+                }
+                
+            } catch (SerialPortException ex) {
+                Logger.getLogger(SerialComm.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public String[] getPorts(){
+        return serialPortNames;
+    }
+    
 }
+
