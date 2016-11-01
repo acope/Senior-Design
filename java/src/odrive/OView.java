8aee5543f12f1873ac6b8edc284a0301898c3cea
Austin <copeman.austin@gmail.com>
2016-11-01 11:26:53 -0400

    Removing unused packages

--- java/src/odrive/OView.java
+++ java/src/odrive/OView.java
@@ -1,10 +1,5 @@
 package odrive;
 
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.JOptionPane;
 import net.miginfocom.swing.MigLayout;
