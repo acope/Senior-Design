a17bc1820b85c31ed59f994bacb43ee98a3247c9
Austin <copeman.austin@gmail.com>
2016-11-03 14:33:33 -0400

    Creation of Up Time

--- java/src/helper/UpTimeCounter.java
+++ java/src/helper/UpTimeCounter.java
@@ -1,7 +1,6 @@
 package helper;
 
-import java.text.*;
-import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Modified from
@@ -10,43 +9,74 @@ import java.util.*;
  * @author modified by Austin Copeman
  */
 public class UpTimeCounter implements Runnable{
-    SimpleDateFormat sdf = new SimpleDateFormat("dd/M/yyyy hh:mm:ss");
+    private long startTime = System.currentTimeMillis();
+    private long currentTime = startTime;
+    private String upTime;
+    private boolean run = false; //True = start False = stop
+    Thread t;;
     
     public UpTimeCounter(){
+        this.t = new Thread(this);
+        run = false;
+        t.start();
         
     }
     
+    @Override
     public void run(){
-        
+        if (run = true){
+            currentTime = System.currentTimeMillis();
+            getDurationBreakdown(currentTime - startTime);
+        }
     }
     
     public void start(){
-        
+        run = true;
+        startTime = System.currentTimeMillis();
+        currentTime = startTime;
     }
     
     public void stop(){
-        
+        run = false;
     }
     
-    public String timeDiff(Date start, Date end){
-        long different = end.getTime() - start.getTime();
-        
-        long secondsMilli = 1000; //Seconds in milliseconds
-        long minutesMilli = secondsMilli * 60; //Minutes in milli
-        long hoursMilli = minutesMilli * 60; //Hours in milli
-        long daysMilli = hoursMilli *24;
-        
-        long elapsedDays = different / daysMilli;
-        different = different % daysMilli;
-        
-        long elapsedHours = different / hoursMilli;
-        different = different % hoursMilli;
-        
-        long elapsedMinutes = different / secondsMilli;
-        
-        String upTime = Long.toString(elapsedDays) + " days" + Long.toString(elapsedHours) + "hours"
-                        + Long.toString(elapsedMinutes) + " minutes";
-        
+    public String getUpTime(){
+        if(run == true){
+            run();
+        }
         return upTime;
+    }     
+    
+       /**
+     * Convert a millisecond duration to a string format
+     * 
+     * @param millis A duration to convert to a string form
+     */
+    private void getDurationBreakdown(long millis)
+    {
+        if(millis < 0)
+        {
+            throw new IllegalArgumentException("Duration must be greater than zero!");
+        }
+
+        long days = TimeUnit.MILLISECONDS.toDays(millis);
+        millis -= TimeUnit.DAYS.toMillis(days);
+        long hours = TimeUnit.MILLISECONDS.toHours(millis);
+        millis -= TimeUnit.HOURS.toMillis(hours);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+        millis -= TimeUnit.MINUTES.toMillis(minutes);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+
+        StringBuilder sb = new StringBuilder(64);
+        sb.append(days);
+        sb.append(" Days ");
+        sb.append(hours);
+        sb.append(" Hours ");
+        sb.append(minutes);
+        sb.append(" Minutes ");
+        sb.append(seconds);
+        sb.append(" Seconds");
+
+        upTime = sb.toString();
     }
 }
