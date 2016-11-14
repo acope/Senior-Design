96aa62dcc6370cb8d7ffbe9d1af5fddf189a7a5c
Austin <copeman.austin@gmail.com>
2016-11-14 12:20:55 -0500

    Added Windows DLL support

--- java/src/odrive/ODrive.java
+++ java/src/odrive/ODrive.java
@@ -2,6 +2,9 @@ package odrive;
 
 import helper.DllLoader;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sun.awt.OSInfo;
 
 /**
  * Main class 
@@ -14,7 +17,24 @@ public class ODrive {
     private static final String PATH_WIN_64 = "/res/winDLLs/64bit/"; 
     
     public static void main(String[] args) throws IOException {
-        DllLoader.extractAndLoad(PATH_WIN_64 + RXTX_SERIAL_DLL);
+        String osArch = System.getProperty("os.arch"); //32bit=x86 64bit=64
+        String osName = System.getProperty("os.name"); //OS used: Windows
+        
+        //Check for windows architecture
+        if(osName.contains("Windows")){
+            Logger.getLogger(ODrive.class.getName()).log(Level.INFO, "Operation System: " + osName, osName);           
+            //check for JVM bit, assumes JVM matches computer
+            if(osArch.contains("86")){
+                //32 bit java
+                DllLoader.extractAndLoad(PATH_WIN_32 + RXTX_SERIAL_DLL);
+                Logger.getLogger(ODrive.class.getName()).log(Level.INFO, "Java Architecture: " + osArch, osArch);  
+            }else{
+                //64 bit java
+                DllLoader.extractAndLoad(PATH_WIN_64 + RXTX_SERIAL_DLL);
+                Logger.getLogger(ODrive.class.getName()).log(Level.INFO, "Java Architecture: " + osArch, osArch);  
+            }
+        }
+        
         OController controller = new OController();
     }
     
